9cb92bdbc2416338cedac56ccbf240cb8c631dd9
MAC-ANTHONY <macanthonygreat@gmail.com>
2023-11-30 11:34:04 +0000

    unused model disabled

--- src/Singlequestion.jsx
+++ src/Singlequestion.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react"
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
+// import { useState } from "react"
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Singlequestion = ({ id, activeId, toggleQuestion, title, info }) => {
   // const [showInfo, setShowInfo] = useState(false)
-  const isActive = id === activeId
+  const isActive = id === activeId;
   // Note: all of them are going to be false because, activeId is null
   return (
-    <article className='question'>
+    <article className="question">
       <header>
         <h5>{title}</h5>
-        <button className='question-btn' onClick={() => toggleQuestion(id)}>
+        <button className="question-btn" onClick={() => toggleQuestion(id)}>
           {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
       {isActive && <p>{info}</p>}
     </article>
-  )
-}
-export default Singlequestion
+  );
+};
+export default Singlequestion;
 
 // import { useState } from "react"
 // import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
